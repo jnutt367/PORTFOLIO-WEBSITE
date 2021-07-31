5757f5eebcd5846a17a339f24169adfd6904ea25
Jason Nutt <jnutt367@gmail.com>
2021-07-31 16:42:56 -0400

    Update default.js

--- src/themes/default.js
+++ src/themes/default.js
@@ -8,8 +8,8 @@ export default {
     colors: {
         primary1: "hsl(204,23.8%,95.9%)",
         background1: "#0F1624",
-        accent1: "hsl(34.9,98.6%,72.9%)",
-        button: "hsl(777.1,100%,36.1%)",
+        accent1: "hsl(0, 96%, 46%)",
+        button: "hsl(206.1,100%,36.1%)",
         background2: "black",
         background3: "radial-gradient( black 0%, black 70% )"
     },
