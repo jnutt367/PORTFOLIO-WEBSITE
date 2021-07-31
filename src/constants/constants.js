326c92475cd8161ce1975e5313756f2ac6c0bde0
Jason Nutt <jnutt367@gmail.com>
2021-07-31 12:04:43 -0400

    Cropped project image
    updated image src to reflect new impot=rted image with cropped borders.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
         {
         title: 'Learn.Build.Type',
         description: "This is a FullStack tying game, utilizing serverless functions, Netlify to host and deploy and AirTable Base as my data storage unit",
-        image: '/images/typingGameImage.jpg',
+        image: '/images/TypingGame.jpg',
         tags: ['React', 'Serverless', 'Netlify'],
         source: 'https://frosty-benz-f55e4b.netlify.app/',
         visit: 'https://github.com/jnutt367/react-serverless-demo.git',
