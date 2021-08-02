466c3bc60edc478779f6a09567f64f34c2b2df43
Jason Nutt <jnutt367@gmail.com>
2021-08-02 11:54:28 -0400

    Update default.js
    Changed aaccent1 color

--- src/themes/default.js
+++ src/themes/default.js
@@ -1,14 +1,14 @@
 export default {
     // Temp fonts
     fonts: {
-        title: "Renault LighT",
+        title: "Renault Light",
         main: " Apex-New"
     },
     // Colors for layout
     colors: {
         primary1: "hsl(204,23.8%,95.9%)",
         background1: "#0F1624",
-        accent1: "hsl(144, 100%, 90%)",
+        accent1: "hsl(0, 0%, 50%)",
         button: "hsl(255, 100%, 90%)",
         background2: "black",
         background3: "radial-gradient( green 0%, black 100% )"
