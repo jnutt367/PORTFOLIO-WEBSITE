ec6b785fe4f4432b941b1d4314d66acd8ca06547
Jason Nutt <jnutt367@gmail.com>
2021-08-03 07:38:33 -0400

    Update Hero.js
    Changed the order of the Hero description

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,13 +12,13 @@ const Hero = (props) => (
         Welcome To<br />
         My Creative Web Portfolio
       </SectionTitle>
- <SectionText style={{  marginLeft: "20px", paddingLeft: "10px" }}>
-      * I also have 15+ years in the workforce, in an industry that is soley based on the satisfaction of the customer!
-   </SectionText>
       <SectionText style={{  marginLeft: "20px", paddingLeft: "10px" }}>
        I utilize a wide variety of best practices in order to provide a wide array of sustainable solutions for cross browser consistency, scalability and reliability.
        This site has just a few of my projects that show a taste of the different technologies I have used to build, publish and deploy.
       </SectionText>
+ <SectionText style={{  marginLeft: "20px", paddingLeft: "10px" }}>
+      * I also have 15+ years in the workforce, in an industry that is soley based on the satisfaction of the customer!
+   </SectionText>
         <Button onClick={() => window.location = 'mailto:jnutt367@gmail.com'}>Contact Me</Button>
     </LeftSection>
   </Section>
