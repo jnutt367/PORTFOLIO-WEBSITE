7af387e9acb9db603442db81b58f269ce88bf01b
Jason Nutt <jnutt367@gmail.com>
2021-09-08 10:07:49 -0400

    Update default.js
    Changing color scheme back to match my current learning journey with MongoDB University. Using my  future Alumni Colors will inspire me to complete the quest.

--- src/themes/default.js
+++ src/themes/default.js
@@ -7,11 +7,11 @@ export default {
     // Colors for layout
     colors: {
         primary1: "white",
-        background1: "pink",
+        background1: "green",
         accent1: "white",
         button: "white",
         background2: "black",
-        background3: "radial-gradient( black 75%, rgba(255, 0, 0, 0.8) 93% )"
+        background3: "radial-gradient( black 85%, rrgba(0, 177, 106, 1) 93% )"
     },
     // Breakpoints for responsive design
     breakpoints: {
