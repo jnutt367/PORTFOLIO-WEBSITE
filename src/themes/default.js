025c2566159a31e2c6d7ab4eca010071fd954a33
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2022-04-13 15:52:41 -0400

    Update default.js
    Gray

--- src/themes/default.js
+++ src/themes/default.js
@@ -11,7 +11,7 @@ export default {
         accent1: "white",
         button: "white",
         background2: "black",
-        background3: "green"
+        background3: "gray"
     },
     // Breakpoints for responsive design
     breakpoints: {
