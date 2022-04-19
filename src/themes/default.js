fc0dee5839356ad51d5af26cbe70a0f15237a639
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2022-04-18 20:00:00 -0400

    Update default.js
    changing layout

--- src/themes/default.js
+++ src/themes/default.js
@@ -11,7 +11,7 @@ export default {
         accent1: "white",
         button: "white",
         background2: "black",
-        background3: "gray"
+        background3: "black"
     },
     // Breakpoints for responsive design
     breakpoints: {
