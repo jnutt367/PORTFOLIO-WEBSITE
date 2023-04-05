080fde5c2e01db7d90093acdfbd0dfb4d2530f55
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-04-04 20:47:13 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     
     <Layout />
-   
+   <>
        <Section grid>
         <Hero />
          <BgAnimation />
@@ -19,7 +19,7 @@ const Home = () => {
       <Technologies />
       <Timeline />
       <Acomplishments />
-   
+   </>
    
   );
 };
