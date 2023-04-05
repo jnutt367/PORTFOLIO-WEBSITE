4be35d6d9e395c23d599b99a2b7a619070d41747
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-04-04 20:48:32 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 const Home = () => {
   return (
-    
+    <>
     <Layout />
-   <>
        <Section grid>
         <Hero />
          <BgAnimation />
